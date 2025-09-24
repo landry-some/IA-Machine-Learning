2b7b7e209ec98546e3e89db6c720ccb629679130
landry-some <landrysome007@gmail.com>
2025-09-24 14:40:47 -0300

    cleaning the negative numbers

--- 02. New York Taxis.py
+++ 02. New York Taxis.py
@@ -6,7 +6,8 @@ df = pd.read_parquet('taxis.parquet') #pip install pyarrow para ler parquet
 
 df = df[['passenger_count', 'trip_distance', 'RatecodeID', 'PULocationID', 'DOLocationID',
          'payment_type', 'fare_amount', 'extra', 'mta_tax', 'tip_amount', 'tolls_amount',
-         'total_amount', 'Airport_fee', 'cbd_congestion_fee']]
+         'improvement_surcharge','total_amount', 'Airport_fee', 'congestion_surcharge',
+         'cbd_congestion_fee']]
 
 df['passenger_count'] = df['passenger_count'].fillna(1)
 df.loc[df['passenger_count'] == 0, 'passenger_count'] = 1
@@ -14,5 +15,12 @@ df = df[df['passenger_count'] <= 5]
 
 df['RatecodeID'] = df['RatecodeID'].fillna(99)
 df['Airport_fee'] = df['Airport_fee'].fillna(0)
+
+df['congestion_surcharge'] = df['congestion_surcharge'].fillna(2.50)
+df = df[(df['congestion_surcharge'] >= 0.0) & (df['fare_amount'] >= 0.0)
+        & (df['extra'] >= 0.0) & (df['mta_tax'] >= 0.0) & (df['tip_amount'] >= 0.0)
+        & (df['tolls_amount'] >= 0.0) & (df['improvement_surcharge'] >= 0.0)
+        & (df['total_amount'] >= 0.0) & (df['Airport_fee'] >= 0.0)
+        & (df['cbd_congestion_fee'] >= 0.0)]
 
 print(df)
