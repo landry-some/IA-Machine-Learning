2c9ae31261c9f43c89e6d1a19d6d9b9baf395404
landry-some <landrysome007@gmail.com>
2025-09-24 14:18:37 -0300

    updated

--- 02. New York Taxis.py
+++ 02. New York Taxis.py
@@ -2,17 +2,16 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-lista = []
-
 df = pd.read_parquet('taxis.parquet') #pip install pyarrow para ler parquet
 
 df = df[['passenger_count', 'trip_distance', 'RatecodeID', 'PULocationID', 'DOLocationID',
          'payment_type', 'fare_amount', 'extra', 'mta_tax', 'tip_amount', 'tolls_amount',
          'total_amount', 'Airport_fee', 'cbd_congestion_fee']]
 
-print(df.isna().sum())
-
 df['passenger_count'] = df['passenger_count'].fillna(1)
+df.loc[df['passenger_count'] == 0, 'passenger_count'] = 1
+df = df[~df['passenger_count'].isin([5, 6, 7, 8, 9])]
+
 df['RatecodeID'] = df['RatecodeID'].fillna(99)
 df['Airport_fee'] = df['Airport_fee'].fillna(df['Airport_fee'].mean().round(2))
 
